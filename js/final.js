1ddcd9b93a9195a4733c87c88cdd9a02581fbd36
babackadibi <babackadibi@babackaibisimac.fios-router.home>
2018-09-23 11:56:06 -0400

    Improving collision detection

--- js/final.js
+++ js/final.js
@@ -71,6 +71,15 @@ rock2 = new Object({
     skin: new Raster("images/rock.png", [1000, 623]),
     speed: [xSpeed, ySpeed]
 });
+transparentBoxLeft = new Object({
+    skin: new Raster("images/transparentBox.png", [50, 320]),
+    speed: [0, 0]
+});
+
+transparentBoxRight = new Object({
+    skin: new Raster("images/transparentBox.png", [1250, 320]),
+    speed: [0, 0]
+});
 
 halfway  = new Object({
     skin: new Raster("images/halfway.png", [1250, 320]),
@@ -83,6 +92,27 @@ halfway  = new Object({
         });
  
 /******************* Game Play ******************/
+function asteroidHit(asteroid1, asteroid2){
+    if(asteroid1.skin.bounds.top == asteroid2.skin.bounds.bottom){
+         asteroid1.speed[1] = Math.abs(asteroid1.speed[1]);
+        asteroid2.speed[1] = -1 * Math.abs(asteroid2.speed[1]);
+    }
+    
+    if(asteroid1.skin.bounds.left == asteroid2.skin.bounds.right){
+         asteroid1.speed[0] = Math.abs(asteroid1.speed[0]);
+        asteroid2.speed[0] = -1 * Math.abs(asteroid2.speed[0]);
+    }
+    
+    if(asteroid1.skin.bounds.bottom == asteroid2.skin.bounds.top){
+         asteroid2.speed[1] = Math.abs(asteroid2.speed[1]);
+        asteroid1.speed[1] = -1 * Math.abs(asteroid1.speed[1]);
+    }
+    
+    if(asteroid1.skin.bounds.right == asteroid2.skin.bounds.left){
+         asteroid2.speed[0] = Math.abs(asteroid2.speed[0]);
+        asteroid1.speed[0] = -1 * Math.abs(asteroid1.speed[0]);
+    }
+}
 function collisionCheck(){
     if(fish.skin.bounds.intersects(p1.skin.bounds) || fish.skin.bounds.intersects(p1.skin.bounds) || rock.skin.bounds.intersects(p1.skin.bounds) || rock.skin.bounds.intersects(p1.skin.bounds) || rock2.skin.bounds.intersects(p1.skin.bounds) || rock2.skin.bounds.intersects(p1.skin.bounds)){
         // turns the fish around if the hit a player     
@@ -104,16 +134,13 @@ function collisionCheck(){
         }
     }
     else if(fish.skin.bounds.intersects(rock.skin.bounds)){
-        fish.speed[0] * -1;
-        rock.speed[0] * -1;
+        asteroidHit(fish, rock);
     } 
     else if(fish.skin.bounds.intersects(rock2.skin.bounds)){
-        fish.speed[0] * -1;
-        rock2.speed[0] * -1;
+        asteroidHit(fish, rock2);
     } 
     else if(rock.skin.bounds.intersects(rock2.skin.bounds)){
-        rock.speed[0] * -1;
-        rock2.speed[0] * -1;
+        asteroidHit(rock, rock2);
     }
         
        
@@ -134,20 +161,56 @@ function collisionCheck(){
     
     
     
-    else if(fish.skin.position.x <= 140 || fish.skin.position.x >= width - 140){
+    else if(fish.skin.bounds.intersects(transparentBoxLeft.skin.bounds)){
        // makes the fish bounce off of the top or bottom wall
-        fish.speed[0] *= -1; 
+        if(fish.speed[0]<0){
+            fish.speed[0] *= -1; 
+            console.log("Fish Collide Left");
+        }
     } 
     
-    else if(rock.skin.position.x <= 140 || rock.skin.position.x >= width - 140){
+     else if(fish.skin.bounds.intersects(transparentBoxRight.skin.bounds)){
        // makes the fish bounce off of the top or bottom wall
-        rock.speed[0] *= -1; 
+        if(fish.speed[0]>0){
+            fish.speed[0] *= -1;
+            console.log("Fish Collide Right");
+        }
     } 
     
-    else if(rock2.skin.position.x <= 140 || rock2.skin.position.x >= width - 140){
+    
+     else if(rock.skin.bounds.intersects(transparentBoxLeft.skin.bounds)){
        // makes the fish bounce off of the top or bottom wall
-        rock2.speed[0] *= -1; 
+        if(rock.speed[0]<0){
+            rock.speed[0] *= -1;
+            console.log("Rock Collide Left");
+        }
     } 
+    
+     else if(rock.skin.bounds.intersects(transparentBoxRight.skin.bounds)){
+       // makes the fish bounce off of the top or bottom wall
+        if(rock.speed[0]>0){
+            rock.speed[0] *= -1;
+            console.log("Rock Collide Right");
+        }
+    } 
+    
+    else if(rock2.skin.bounds.intersects(transparentBoxLeft.skin.bounds)){
+       // makes the fish bounce off of the top or bottom wall
+        if(rock2.speed[0]<0){
+            rock2.speed[0] *= -1;
+            console.log("Rock2 Collide Left");
+        }
+    } 
+    
+     else if(rock2.skin.bounds.intersects(transparentBoxRight.skin.bounds)){
+       // makes the fish bounce off of the top or bottom wall
+        if(rock2.speed[0]>0){
+            rock2.speed[0] *= -1;
+            console.log("Rock2 Collide Right");
+        }
+    } 
+    
+    
    
     
   if(halfway.skin.bounds.intersects(p1.skin.bounds)){
@@ -166,261 +229,243 @@ function collisionCheck(){
          rock2.speed[1] = 3;
            }
                if(level == 2){
-        fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+        
         fish.speed[1] = 4;
          rock.speed[1] = 4;
          rock2.speed[1] = 4;
            }
            
                if(level == 3){
-  fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+  
         fish.speed[1] = 5;
          rock.speed[1] = 5;
          rock2.speed[1] = 5;
            }
            
            if(level == 4){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+                 
         fish.speed[1] = 6;
          rock.speed[1] = 6;
          rock2.speed[1] = 6;
            }
            
            if(level == 5){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+                
         fish.speed[1] = 7;
          rock.speed[1] = 7;
          rock2.speed[1] = 7;
            }
            
            if(level == 6){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+
         fish.speed[1] = 8;
          rock.speed[1] = 8;
          rock2.speed[1] = 8;
            }
            
            if(level == 7){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+
         fish.speed[1] = 9;
          rock.speed[1] = 9;
          rock2.speed[1] = 9;
            }
            
            if(level == 8){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
         fish.speed[1] = 10;
          rock.speed[1] = 10;
          rock2.speed[1] = 10;
            }
            
            if(level == 9){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
         fish.speed[1] = 11;
          rock.speed[1] = 11;
          rock2.speed[1] = 11;
            }
            
            if(level == 10){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 12;
          rock.speed[1] = 12;
          rock2.speed[1] = 12;
            }
            
            if(level == 11){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 13;
          rock.speed[1] = 13;
          rock2.speed[1] = 13;
            }
            
            if(level == 12){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 14;
          rock.speed[1] = 14;
          rock2.speed[1] = 14;
            }
            
            if(level == 13){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 15;
          rock.speed[1] = 15;
          rock2.speed[1] = 15;
            }
            
            if(level == 14){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 16;
          rock.speed[1] = 16;
          rock2.speed[1] = 16;
            }
            
            if(level == 15){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 17;
          rock.speed[1] = 17;
          rock2.speed[1] = 17;
            }
            
            if(level == 16){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 18;
          rock.speed[1] = 18;
          rock2.speed[1] = 18;
            }
            
            if(level == 17){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 19;
          rock.speed[1] = 19;
          rock2.speed[1] = 19;
            }
            
            if(level == 18){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 20;
          rock.speed[1] = 20;
          rock2.speed[1] = 20;
            }
            
            if(level == 19){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 21;
          rock.speed[1] = 21;
          rock2.speed[1] = 21;
            }
            
            if(level == 20){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 22;
          rock.speed[1] = 22;
          rock2.speed[1] = 22;
            }
            
            if(level == 21){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 23;
          rock.speed[1] = 23;
          rock2.speed[1] = 23;
            }
            
            if(level == 22){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 24;
          rock.speed[1] = 24;
          rock2.speed[1] = 24;
            }
            
            if(level == 23){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 25;
          rock.speed[1] = 25;
          rock2.speed[1] = 25;
            }
            
            if(level == 24){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 26;
          rock.speed[1] = 26;
          rock2.speed[1] = 26;
            }
            
            if(level == 25){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 27;
          rock.speed[1] = 27;
          rock2.speed[1] = 27;
            }
            
            if(level == 26){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 28;
          rock.speed[1] = 28;
          rock2.speed[1] = 28;
            }
            
            if(level == 27){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 29;
          rock.speed[1] = 29;
          rock2.speed[1] = 29;
            }
            
            if(level == 28){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 30;
          rock.speed[1] = 30;
          rock2.speed[1] = 30;
            }
            
            if(level == 29){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 31;
          rock.speed[1] = 31;
          rock2.speed[1] = 31;
            }
            
            if(level == 30){
-                 fish.position = new Point(300, mid);
-        rock.position = new Point(650, 5);
-        rock2.position = new Point(1000, 623);
+ 
+ 
+ 
         fish.speed[1] = 32;
          rock.speed[1] = 32;
          rock2.speed[1] = 32; 
